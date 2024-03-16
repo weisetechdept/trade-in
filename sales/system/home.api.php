a03d8c2405a101b17b7454ec07ce036bb16ccf24
Nattanon Kh <nattanondev@gmail.com>
2024-03-16 14:13:09 +0700

    auth v1.1

--- sales/system/home.api.php
+++ sales/system/home.api.php
@@ -2,10 +2,10 @@
     session_start();
     require_once '../../db-conn.php';
     date_default_timezone_set("Asia/Bangkok");
-    
+    $user_id = $_SESSION['tin_user_id'];
 
     $db->join('car_stock c', "f.find_id=c.cast_car", "INNER");
-    $stock = $db->where('c.cast_sales_parent_no','1')->get("finance_data f", null ,"c.cast_id,c.cast_license,f.find_brand,f.find_serie,f.find_section,c.cast_color,c.cast_price,c.cast_sales_parent,c.cast_sales_team,c.cast_status");
+    $stock = $db->where('c.cast_sales_parent_no',$user_id)->get("finance_data f", null ,"c.cast_id,c.cast_license,f.find_brand,f.find_serie,f.find_section,c.cast_color,c.cast_price,c.cast_sales_parent,c.cast_sales_team,c.cast_status");
 
     if(!empty($stock)){
         foreach ($stock as $value) {
