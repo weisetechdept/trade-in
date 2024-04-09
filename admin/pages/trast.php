6b8031773939e23ec54d7a9c571e7cadd95a521d
Nattanon Kh <nattanondev@gmail.com>
2024-04-09 14:32:47 +0700

    delete memu v0.2

--- admin/pages/trast.php
+++ admin/pages/trast.php
@@ -96,70 +96,6 @@
                         </div>
                     </div>  
 
-                <div class="row" id="count">
-                    <div class="col-12 col-lg-3">
-                        <div class="row">
-                            <div class="col-12 mb-2">
-                                <a href="#">
-                                    <div class="card bg-danger border-danger">
-                                        <div class="card-body">
-                                            <div class="mb-2">
-                                                <h5 class="card-title mb-0 text-white">รถยนต์ทั้งหมด</h5>
-                                            </div>
-                                            <div class="row d-flex align-items-center mb-2">
-                                                <div class="col-8">
-                                                    <h2 class="d-flex align-items-center text-white mb-0">
-                                                        {{ count }}
-                                                    </h2>
-                                                </div>
-                                            </div>
-
-                                        </div>
-                                    </div>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-12 col-lg-9">
-                        <div class="card">
-                            <div class="card-body">
-                                
-                                    <div class="form-row">
-                                        <div class="col-md-3 mb-1">
-                                            <label>ตั้งแต่</label>
-                                            <input type="date" class="form-control" v-model="search.start">
-                                        </div>
-
-                                        <div class="col-md-3">
-                                            <label>ถึง</label>
-                                            <input type="date" class="form-control" v-model="search.end">
-                                        </div>
-
-                                        <div class="col-md-3">
-                                            <label>สถานะ</label>
-                                            <div class="input-group">
-                                                <select class="form-control" v-model="search.status">
-                                                    <option value="all">ทุกสถานะ</option>
-                                                    <option value="0">ไม่มีสถานะ</option>
-                                                    <option value="1">ติดตามลูกค้า</option>
-                                                    <option value="2">ไม่ได้สัมผัสรถ</option>
-                                                    <option value="3">ลูกค้าขายเอง / ขายที่อื่น</option>
-                                                    <option value="4">สำเร็จ</option>
-                                                </select>
-                                            </div>
-                                        </div>
-
-                                        <div class="col-md-3">
-                                            <button class="btn btn-primary search-btn" @click="searchData()" type="submit">ค้นหา</button>
-                                        </div>
-                                    </div>
-                          
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
                     <div class="row">
                         <div class="col-12">
                             <div class="card">
@@ -322,40 +258,6 @@
                     }
                 }
             ],
-        });
-
-        var count = new Vue({
-            el: '#count',
-            data: {
-                count: 0,
-                search: {
-                    start: '<?php echo date('Y-m-01'); ?>',
-                    end: '<?php echo date('Y-m-d'); ?>',
-                    status: 'all'
-                }
-            },
-            mounted() {
-                axios.get('/admin/system/home.api.php?get=count')
-                    .then(response => (
-                        this.count = response.data.count
-                    ));
-            },
-            methods: {
-                searchData() {
-                    swal({
-                        title: "กำลังโหลดข้อมูล",
-                        text: "โปรดรอสักครู่...",
-                        icon: "info",
-                        buttons: false,
-                        closeOnClickOutside: false,
-                        closeOnEsc: false
-                    });
-                    $('#datatable').DataTable().ajax.url('/admin/system/home.api.php?get=search&start='+this.search.start+'&end='+this.search.end+'&status='+this.search.status).load(function() {
-                        swal.close(); // Close the loading message
-                    });
-                    this.count = $('#datatable').DataTable().rows().count();
-                }
-            }
         });
 
     </script>
