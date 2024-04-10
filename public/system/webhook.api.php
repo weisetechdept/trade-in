01844c9dcbcf9f85bdc6937253921f274c5d55db
Nattanon Kh <nattanondev@gmail.com>
2024-04-10 14:41:31 +0700

    Line OA v0.3

--- public/system/webhook.api.php
+++ public/system/webhook.api.php
@@ -3,24 +3,6 @@
 session_start();
 require_once '../../db-conn.php';
 date_default_timezone_set("Asia/Bangkok");
-
-function login($uid) {
-  global $db;
-  $chk = $db->where('user_line_uid', $uid)->getOne('user');
-  if($chk) {
-
-  } else {
-    $data = array(
-      'user_nickname' => 'rand-'.rand(100000, 999999),
-      'user_line_uid' => $uid,
-      'user_line_img' => '',
-      'user_permission' => 0,
-      'user_status' => 1,
-      'user_created' => date('Y-m-d H:i:s')
-    );
-    $db->insert('user', $data);
-  }
-}
  
 $strAccessToken = "IZd/+LM0eFbZBGVq67BcM6AC8MDkZSi7/DsikGWU45/a2moikJuzGP77d8J3w1UOFcc98ku2MmnnQwnKwYOyAWvkuMScEfxrImfS5NrC+nRX/bzJNehiCX9PwezVE3St1i81+6WuMUj90anooQivAAdB04t89/1O/w1cDnyilFU=";
 $content = file_get_contents('php://input');
@@ -34,7 +16,27 @@ $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
 
 if($arrJson['events'][0]['message']['text'] == "[ระบบ] ประเมินราคา"){
   $usrid = $arrJson['events'][0]['source']['userId'];
-  login($usrid);
+
+  $chk = $db->where('user_line_uid', $uid)->getOne('user');
+  if($chk) {
+
+  } else {
+    $data = array(
+      'user_nickname' => 'rand-'.rand(100000, 999999),
+      'user_line_uid' => $uid,
+      'user_line_img' => '',
+      'user_permission' => 0,
+      'user_status' => 1,
+      'user_created' => date('Y-m-d H:i:s')
+    );
+    $update = $db->insert('user', $data);
+    if($update) {
+      $arrPostData = array();
+      $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+      $arrPostData['messages'][0]['type'] = "text";
+      $arrPostData['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$arrJson['events'][0]['source']['userId'];
+    }
+  }
 }
  /*
 if($arrJson['events'][0]['message']['text'] == "[ระบบ] ประเมินราคา"){
