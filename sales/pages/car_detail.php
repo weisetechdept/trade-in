69b8d83bad13095fb55841b6026eb9d4350c50f1
Nattanon Kh <nattanondev@gmail.com>
2024-06-12 13:50:35 +0700

    upload multiple file v0.3

--- sales/pages/car_detail.php
+++ sales/pages/car_detail.php
@@ -350,7 +350,7 @@
                                       
                                             <div class="row">
                                                 <div class="col form-group mt-2">
-                                                    <input type="file" class="form-control file-upload" name="uploadfiles" id="uploadfiles" ref="uploadfiles" multiple accept="image/*">
+                                                    <input type="file" class="form-control file-upload" id="uploadfiles" ref="uploadfiles" multiple />
                                                 </div>
                                             </div>
 
