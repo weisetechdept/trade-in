a80b5e64bfd79e104bcf195c62274dbc7705c600
Nattanon Kh <nattanondev@gmail.com>
2024-06-20 11:17:18 +0700

    update admin member

--- admin/system/member.api.php
+++ admin/system/member.api.php
@@ -3,16 +3,23 @@
     require_once '../../db-conn.php';
     date_default_timezone_set("Asia/Bangkok");
 
-    $member = $db->get('user');
-    foreach ($member as $value) {
-        $api['data'][] = array(
-            $value['user_id'],
-            $value['user_line_img'],
-            $value['user_nickname'],
-            $value['user_permission'],
-            $value['user_status'],
-            date('d/m/Y H:i:s', strtotime($value['user_datetime']))
-        );
-    }
+    if($_SESSION['tin_admin'] != true){
+        $api = array('status' => 'error', 'message' => 'Permission denied');
+    } else {
 
+        $member = $db->get('user');
+        foreach ($member as $value) {
+            $api['data'][] = array(
+                $value['user_id'],
+                $value['user_line_img'],
+                $value['user_nickname'],
+                $value['user_permission'],
+                $value['user_status'],
+                date('d/m/Y H:i:s', strtotime($value['user_datetime']))
+            );
+        }
+
+    }
     echo json_encode($api);
+
+    
