d17a6cd9275391ee9d1ac721380c0d2c096014d0
Nattanon Kh <nattanondev@gmail.com>
2024-07-02 14:27:01 +0700

    partner v16

--- partner/system/noti_offer.inc.php
+++ partner/system/noti_offer.inc.php
@@ -52,7 +52,7 @@
             curl_setopt( $chOne, CURLOPT_SSL_VERIFYHOST, 0); 
             curl_setopt( $chOne, CURLOPT_SSL_VERIFYPEER, 0); 
             curl_setopt( $chOne, CURLOPT_POST, 1); 
-            curl_setopt( $chOne, CURLOPT_POSTFIELDS, "message=".$sMessage); 
+            curl_setopt( $chOne, CURLOPT_POSTFIELDS, "message=".json_encode($sMessage)); 
             $headers = array( 'Content-type: application/x-www-form-urlencoded', 'Authorization: Bearer '.$sToken.'', );
             curl_setopt($chOne, CURLOPT_HTTPHEADER, $headers); 
             curl_setopt( $chOne, CURLOPT_RETURNTRANSFER, 1); 
