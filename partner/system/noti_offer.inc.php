ce96fa7c8307ed16ed50f1857c556e1e256970d8
Nattanon Kh <nattanondev@gmail.com>
2024-07-02 15:35:42 +0700

    partner v17

--- partner/system/noti_offer.inc.php
+++ partner/system/noti_offer.inc.php
@@ -18,45 +18,18 @@
             error_reporting(E_ALL);
 
             $sToken = "8PejR1DTTI8B8rEb8STbW2bZs8FDAtA21Ll7nBO7Hmf";
-            /*
-                $sMessage = "พันธมิตร $partner ให้ราคารหัสรถ ID : $carid ราคา $price บาท";
-            */
-            $sMessage = array(
-                'type' => 'template',
-                'altText' => 'ให้ราคาจากพันธมิตร',
-                'template' => array(
-                    "type" => "buttons",
-                    "thumbnailImageUrl" => 'https://dummyimage.com/600x400/000/fff',
-                    "imageAspectRatio" => "rectangle",
-                    "imageSize" => "cover",
-                    "imageBackgroundColor" => "#FFFFFF",
-                    "title" => "เสนอราคาจากพันธมิตร",
-                    "text" => "พันธมิต $partner รประเมิน ".number_format($price)." บาท ต้องการคุยรายละเอียดกดปุ่มด้านล่าง",
-                    "defaultAction" => array(
-                        "type" => "uri",
-                        "label" => "View detail",
-                        "uri" => 'https://dummyimage.com/600x400/000/fff'
-                    ),
-                    "actions" => array(
-                        array(
-                            "type" => "uri",
-                            "label" => "ดูข้อมูลรถยนต์",
-                            "uri" => "https://trade-in.toyotaparagon.com/app?way=car&id=".$carid
-                        )
-                    )
-                )
-            );
+            $sMessage = "พันธมิตร $partner ให้ราคารหัสรถ ID : $carid ราคา $price บาท []";
 
             $chOne = curl_init(); 
             curl_setopt( $chOne, CURLOPT_URL, "https://notify-api.line.me/api/notify"); 
             curl_setopt( $chOne, CURLOPT_SSL_VERIFYHOST, 0); 
             curl_setopt( $chOne, CURLOPT_SSL_VERIFYPEER, 0); 
             curl_setopt( $chOne, CURLOPT_POST, 1); 
-            curl_setopt( $chOne, CURLOPT_POSTFIELDS, "message=".json_encode($sMessage)); 
+            curl_setopt( $chOne, CURLOPT_POSTFIELDS, "message=".$sMessage); 
             $headers = array( 'Content-type: application/x-www-form-urlencoded', 'Authorization: Bearer '.$sToken.'', );
             curl_setopt($chOne, CURLOPT_HTTPHEADER, $headers); 
             curl_setopt( $chOne, CURLOPT_RETURNTRANSFER, 1); 
-            $result = curl_exec( $chOne ); 
+            $result = curl_exec( $chOne );
 
         }
 
