c6b72ff76aa2a6a0bbfd036a0fa2e985b40bd414
Nattanon Kh <nattanondev@gmail.com>
2024-07-05 17:02:32 +0700

    update fixed v7

--- partner/pages/home.php
+++ partner/pages/home.php
@@ -76,12 +76,6 @@
             padding: 3px;
         }
         .overlay-sold {
-            background-color: rgba(0, 0, 0, 0.5);
-            position: absolute;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
         }
     </style>
 
