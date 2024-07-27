81909dfb7da80b8d54fd0fb39d9ec8307acea06d
unknown <weisedev.dept@gmail.com>
2024-07-27 16:40:28 +0700

    update search report v0.96

--- admin/system/qm-report.api.php
+++ admin/system/qm-report.api.php
@@ -103,7 +103,7 @@
 
         $trade = $db->where('cast_sales_team', $manager)
                     ->where('cast_datetime', array($start, $end), 'BETWEEN')
-                    ->where('cast_status', array(1,2,3,4),'IN')
+                    ->where('cast_status', array(0,1,2,3,4),'IN')
                     ->getValue('car_stock', 'count(*)');
         
         $per = number_format(($trade / $count) * 100).'%';
