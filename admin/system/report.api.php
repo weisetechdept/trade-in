3053d0fddc0e052647d1600c5eb3b2b582611882
unknown <weisedev.dept@gmail.com>
2024-07-27 14:38:10 +0700

    update search report v0.9

--- admin/system/report.api.php
+++ admin/system/report.api.php
@@ -2,13 +2,20 @@
         session_start();
         require_once '../../db-conn.php';
         date_default_timezone_set("Asia/Bangkok");
-
+/*
         if($_SESSION['tin_admin'] != true){
             header("location: /404");
             exit();
         } else {
-
+*/
             if($_GET['get'] == 'count'){
+                $allst0 = 0;
+                $allst1 = 0;
+                $allst2 = 0;
+                $allst3 = 0;
+                $allst4 = 0;
+                $allst = 0;
+                
                 $team = $db_nms->get('db_user_group');
                 foreach ($team as $value) {
                     if($value['id'] != '26' && $value['id'] != '27' && $value['id'] !== '32'){ //out of team 26,27
@@ -20,7 +27,6 @@
                         $st2 = $db->where('cast_sales_parent_no',$mteam,'IN')->where('cast_status',2)->getValue('car_stock','count(*)');
                         $st3 = $db->where('cast_sales_parent_no',$mteam,'IN')->where('cast_status',3)->getValue('car_stock','count(*)');
                         $st4 = $db->where('cast_sales_parent_no',$mteam,'IN')->where('cast_status',4)->getValue('car_stock','count(*)');
-    
     
                         $api['data'][] = array(
                             $value['name'],
@@ -53,8 +59,15 @@
                 );
     
             } elseif($_GET['get'] == 'search'){
-                $start = $_GET['start'].' 00:00:00';
-                $end = $_GET['end'].' 23:59:59';
+                $allst0 = 0;
+                $allst1 = 0;
+                $allst2 = 0;
+                $allst3 = 0;
+                $allst4 = 0;
+                $allst = 0;
+
+                $start = $_GET['start'];
+                $end = $_GET['end'];
     
                 $team = $db_nms->get('db_user_group');
     
@@ -101,8 +114,8 @@
                 );
             }
             echo json_encode($api);
-
+/*
         }
-
+*/
         
         
