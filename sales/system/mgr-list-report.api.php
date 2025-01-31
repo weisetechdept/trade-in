7711ade9ac139eaa9df6ab2b568b5f410dd0d020
Nattanon Kh <weisetechinfo@gmail.com>
2025-01-31 17:06:45 +0700

    fix: update date range in mgr-list-report API to fixed values and change join type to RIGHT

--- sales/system/mgr-list-report.api.php
+++ sales/system/mgr-list-report.api.php
@@ -29,9 +29,12 @@
         $month = $_GET['month'];
         $year = $_GET['year'];
 
-        $date_form = $_GET['year'].'-'. $_GET['month'].'-01';
-        $date_to = $_GET['year'].'-'. $_GET['month'].'-31';
-    
+        // $date_form = $_GET['year'].'-'. $_GET['month'].'-01';
+        // $date_to = $_GET['year'].'-'. $_GET['month'].'-31';
+
+        $date_form = '2025-01-01';
+        $date_to = '2025-01-31';
+        
         $db->join('car_stock c', "f.find_id=c.cast_car", "RIGHT");
 
         //$db->where('c.cast_datetime', Array($year.'-'.$month.'-01', $year.'-'.$month.'-31'), 'BETWEEN');
