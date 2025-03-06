07f7caed8cef01a608bef9be594952e5dbdf4323
Nattanon Kh <weisetechinfo@gmail.com>
2025-03-06 12:10:23 +0700

    feat: add success report API and update sidebar for navigation

---
+++ admin/pages/report-success.php
@@ -0,0 +1,298 @@
+<?php
+    session_start();
+    if($_SESSION['tin_admin'] != true){
+        header("location: /404");
+        exit();
+    } else {
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Trade-In Success Report</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta content="A77" name="description" />
+    <meta content="A77" name="author" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="/assets/images/favicon.ico">
+
+    <!-- Plugins css -->
+    <link href="/assets/plugins/datatables/dataTables.bootstrap4.css" rel="stylesheet" type="text/css" />
+    <link href="/assets/plugins/datatables/responsive.bootstrap4.css" rel="stylesheet" type="text/css" />
+    <link href="/assets/plugins/datatables/buttons.bootstrap4.css" rel="stylesheet" type="text/css" />
+    <link href="/assets/plugins/datatables/select.bootstrap4.css" rel="stylesheet" type="text/css" />
+
+    <link href="https://fonts.googleapis.com/css2?family=Chakra+Petch:wght@100;200;300;400;500;600;700;800&family=Kanit:wght@100;200;300;400;500;600;700;800&display=swap" rel="stylesheet">
+
+    <!-- App css -->
+    <link href="/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <link href="/assets/css/theme.min.css" rel="stylesheet" type="text/css" />
+    <style>
+        body {
+            font-family: 'Chakra Petch', sans-serif;
+        }
+        .h1, .h2, .h3, .h4, .h5, .h6, h1, h2, h3, h4, h5, h6 {
+            font-family: 'Kanit', sans-serif;
+            font-weight: 400;
+        }
+        .page-content {
+            padding: calc(70px + 24px) calc(5px / 2) 70px calc(5px / 2);
+        }
+        .table {
+            width: 100% !important;
+        }
+        .dtr-details {
+            width: 100%;
+        }
+        .card-body {
+            padding: 1rem;
+        }
+        .card {
+            margin-bottom: 10px;
+        }
+        .btn-group, .btn-group-vertical {
+            margin-bottom: 15px;
+        }
+        .search-btn {
+            margin-top: 27px;
+        }
+        .car-thumb {
+            width: 85px;
+            height: 60px;
+            object-fit: cover;
+            border-radius: 5px;
+        }
+        .badge-soft-unknow {
+            background-color: #f6b9f7;
+            color: #fff;
+        }
+        @media (min-width: 768px) {
+            .page-content {
+                padding: calc(70px + 24px) calc(5px / 2) 70px calc(5px / 2);
+            }
+            div.dataTables_wrapper div.dataTables_filter {
+                text-align: right;
+                float: right;
+            }
+        }
+       
+    </style>
+</head>
+
+<body>
+    <div id="layout-wrapper">
+        <?php 
+                include_once('inc-pages/nav.php');
+                include_once('inc-pages/sidebar.php');
+        ?>
+        <div class="main-content">
+
+            <div class="page-content">
+                <div class="container-fluid">
+
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="page-title-box d-flex align-items-center justify-content-between">
+                                <h4 class="mb-0 font-size-18">รายงานรถที่ซื้อสำเร็จ</h4>
+
+                                <div class="page-title-right">
+                                    <ol class="breadcrumb m-0">
+                                        <li class="breadcrumb-item"><a href="javascript: void(0);">Trade-in</a></li>
+                                        <li class="breadcrumb-item active">รายงานรถที่ซื้อสำเร็จ</li>
+                                    </ol>
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </div>  
+        <div id="app">
+                    <!-- Modal -->
+                    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">e-Card</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <table class="table table-bordered">
+                                    <tbody>
+                                        <tr>
+                                            <th scope="row">1</th>
+                                            <td>{{ ecard.car }}</td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">2</th>
+                                            <td>{{ ecard.section }}</td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">3</th>
+                                            <td>{{ ecard.gen }}</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">ปิด</button>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="card">
+                                <div class="card-body">
+                                    <table id="datatable" class="table table-responsive">
+                                        <thead>
+                                            <tr>
+                                                <th width="45px">รหัส</th>
+                                                <th>รูป</th>
+                                                <th>แบบรุ่น</th>
+                                                <th>ปีรุ่น</th>
+                                                <th>สี</th>
+                                                <th>เซลล์</th>
+                                                <th>ทีม</th>
+                                                <th>วันที่เริ่ม</th>
+                                                <th>จัดการ</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+
+                                </div> 
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+            <footer class="footer">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            2023 © Weise Tech.
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="text-sm-right d-none d-sm-block">
+                                Design & Develop by Weise Tech
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+
+        </div>
+      
+    </div>
+
+    <div class="menu-overlay"></div>
+
+    <script src="/assets/js/jquery.min.js"></script>
+    <script src="/assets/js/bootstrap.bundle.min.js"></script>
+    <script src="/assets/js/metismenu.min.js"></script>
+    <script src="/assets/js/waves.js"></script>
+
+    <script src="/assets/plugins/datatables/jquery.dataTables.min.js"></script>
+    <script src="/assets/plugins/datatables/dataTables.bootstrap4.js"></script>
+    <script src="/assets/plugins/datatables/dataTables.responsive.min.js"></script>
+    <script src="/assets/plugins/datatables/responsive.bootstrap4.min.js"></script>
+    <script src="/assets/plugins/datatables/dataTables.buttons.min.js"></script>
+    <script src="/assets/plugins/datatables/buttons.bootstrap4.min.js"></script>
+    <script src="/assets/plugins/datatables/buttons.html5.min.js"></script>
+    <script src="/assets/plugins/datatables/buttons.flash.min.js"></script>
+    <script src="/assets/plugins/datatables/buttons.print.min.js"></script>
+    <script src="/assets/plugins/datatables/dataTables.keyTable.min.js"></script>
+    <script src="/assets/plugins/datatables/dataTables.select.min.js"></script>
+    <script src="/assets/plugins/datatables/pdfmake.min.js"></script>
+    <script src="/assets/plugins/datatables/vfs_fonts.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.10/vue.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.19.1/axios.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
+ 
+    <script>
+        
+        var app = new Vue({
+            el: '#app',
+            data: {
+                ecard: {
+                    car: '1',
+                    section: '2',
+                    gen: '3'
+                }
+            },
+            mounted: function(){
+                // this.getData();
+                // this.initEventListeners();
+                this.getData();
+            },
+            methods: {
+                getEcard(event) {
+                    let ecardId = event.target.getAttribute('data-ecard');
+                    //swal(`Good job! ${ecardId}`, "You clicked the button!", "success");
+                    $('#exampleModal').modal('show');
+                },
+                getData(){
+                    $('#datatable').DataTable({
+                        ajax: '/admin/system/report-success.api.php',
+                        pageLength: 10,
+                        lengthMenu: [[10, 25, 50, 100], [10, 25, 50, 100]],
+                        processing: true,
+                        serverSide: true,
+                        responsive: true,
+                        bInfo: false,
+                        order: [[0, "desc"]],
+                        language: {
+                            "processing": "กำลังดาวน์โหลดข้อมูล...",
+                            "search": "ค้นหา:",
+                            "lengthMenu": "แสดง _MENU_ รายการ",
+                        },
+                        dom: 'lBfrtip',
+                        buttons: [
+                            'copy', 'print'
+                        ],
+                        search: {
+                            "regex": true,
+                            "smart": false,
+                        },
+                    });
+                },
+                initEventListeners() {
+                    document.addEventListener('click', (event) => {
+                        if (event.target.classList.contains('ecard-btn')) {
+                            this.getEcard(event);
+                        }
+                    });
+                }
+            }
+        });
+
+    </script>
+    
+    <script src="/assets/js/theme.js"></script>
+
+</body>
+
+</html>
+
+<?php } ?>
---
+++ admin/system/report-success.api.php
@@ -0,0 +1,132 @@
+<?php 
+    session_start();
+    require_once '../../db-conn.php';
+    date_default_timezone_set("Asia/Bangkok");
+
+    function DateThai($strDate)
+    {
+        $strYear = date("y",strtotime($strDate));
+        $strMonth= date("n",strtotime($strDate));
+        $strDay= date("j",strtotime($strDate));
+    
+        $strMonthCut = Array("","ม.ค.","ก.พ.","มี.ค.","เม.ย.","พ.ค.","มิ.ย.","ก.ค.","ส.ค.","ก.ย.","ต.ค.","พ.ย.","ธ.ค.");
+        $strMonthThai=$strMonthCut[$strMonth];
+        return "$strDay $strMonthThai $strYear";
+    }
+
+    function getTeamName($uid){
+        global $db_nms;
+        $team = $db_nms->get('db_user_group', null, 'name,detail,leader');
+        foreach($team as $t){
+            $tm = array_merge(json_decode($t['detail']),json_decode($t['leader']));
+            if(in_array($uid,$tm)){
+                return $t['name'];
+            } 
+        }
+    }
+
+    function getSaleName($uid){
+        global $db_nms;
+        $sale = $db_nms->where('id', $uid)->getOne('db_member', null,'first_name');
+        return $sale ? $sale['first_name'] : 'Unknown';
+    }
+
+    function thumb($uid){
+        global $db;
+        $thumb = $db->where('cari_id ', $uid)->getOne('car_image', null,'cari_link');
+        if(empty($thumb)){
+            return '<img src="https://dummyimage.com/600x400/c4c4c4/fff&amp;text=no-image" class="car-thumb">';
+        }else {
+            return "<img src=\"" . $thumb['cari_link'] . "\" class=\"car-thumb\">";
+        }
+    }
+
+    function getBrandSerie($uid){
+        global $db;
+        $brand = $db->where('find_id', $uid)->getOne('finance_data', null,'find_brand ,find_serie');
+        return $brand['find_brand'].' '.$brand['find_serie'];
+    }
+
+    
+    $sql_details_1 = ['user'=> $usern,'pass'=> $passn,'db'=> $dbn,'host'=> $hostn,'charset'=>'utf8'];
+    
+    require 'ssp.class.php';
+    $table = 'car_stock';
+
+    $primaryKey = 'cast_id';
+    $columns = [
+        ['db' => 'cast_id', 'dt' => 0, 'field' => 'cast_id',
+            'formatter' => function($d, $row){
+                return $d;
+            }
+        ],
+        ['db' => 'cast_id', 'dt' => 1, 'field' => 'cast_id',
+            'formatter' => function($d, $row){
+                return thumb($d);
+            }
+        ],
+        ['db' => 'cast_id', 'dt' => 2, 'field' => 'cast_id',
+            'formatter' => function($d, $row){
+                return getBrandSerie($d);
+            }
+        ],
+        ['db' => 'find_year', 'dt' => 3, 'field'=> 'find_year',
+            'formatter' => function($d, $row){
+                return $d;
+            }
+        ],
+        ['db' => 'cast_color', 'dt' => 4, 'field'=> 'cast_color',
+            'formatter' => function($d, $row){
+                return $d; 
+            }
+        ],
+        ['db' => 'cast_sales_parent_no', 'dt' => 5, 'field'=> 'cast_sales_parent_no',
+            'formatter' => function($d, $row){
+                return getSaleName($d); 
+            }
+        ],
+        ['db' => 'cast_sales_parent_no', 'dt' => 6, 'field'=> 'cast_sales_parent_no',
+            'formatter' => function($d, $row){
+                return getTeamName($d); 
+            }
+        ],
+        ['db' => 'cast_datetime', 'dt' => 7, 'field'=> 'cast_datetime',
+            'formatter' => function($d, $row){
+                return DateThai($d); 
+            }
+        ],
+        ['db' => 'cast_id', 'dt' => 8, 'field'=> 'cast_id',
+            'formatter' => function($d, $row){
+                return "<a href=\"/admin/detail/$d\"  target=\"_blank\" class=\"btn btn-outline-primary btn-sm mr-2\"><span class=\"mdi mdi-account-edit\"></span> ข้อมูล</a>";
+            }        
+        ]
+    ];
+
+    // $db->join('finance_data f', 's.cast_car = f.find_id', 'LEFT');
+    // $db->groupBy('cast_id');
+    // $success = $db->where('cast_status', 4)->get('car_stock s');
+
+    // $api = array();
+
+    // foreach($success as $value) {
+    //     $api['data'][] = array(
+    //         $value['cast_id'],
+    //         $value['find_brand'],
+    //         isset($value['find_year']) ? $value['find_year'] : 'ไม่มีข้อมูล',
+    //         isset($value['find_color']) ? $value['find_color'] : 'ไม่มีข้อมูล',
+    //         date('d/m/Y', strtotime($value['cast_datetime'])),
+    //     );
+    // }
+
+    $joinQuery = "FROM car_stock s LEFT JOIN finance_data f ON s.cast_car = f.find_id";
+    
+    $where = " s.cast_status IN (4)";
+
+    if(isset($_GET['search']['value'])){
+        $searchValue = $_GET['search']['value'];
+        $joinQuery .= " AND (s.cast_id LIKE '%$searchValue%' OR s.cast_sales_parent_no LIKE '%$searchValue%' OR f.find_section LIKE '%$searchValue%' OR s.cast_year LIKE '%$searchValue%' OR s.cast_license LIKE '%$searchValue%' OR s.cast_color LIKE '%$searchValue%' OR s.cast_price LIKE '%$searchValue%' OR s.cast_status LIKE '%$searchValue%' OR s.cast_datetime LIKE '%$searchValue%')";
+    }
+    
+    echo json_encode(
+        SSP::simple($_GET, $sql_details_1, $table, $primaryKey, $columns, $joinQuery, $where)
+    );
