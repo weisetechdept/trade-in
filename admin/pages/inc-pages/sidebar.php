16ef9d6473d7750e56577be6ac334dbe8cfc28d2
Nattanon Kh <weisetechinfo@gmail.com>
2025-03-12 12:02:51 +0700

    feat: update sidebar link text for success report

--- admin/pages/inc-pages/sidebar.php
+++ admin/pages/inc-pages/sidebar.php
@@ -41,7 +41,7 @@
                         <li><a href="/admin/qm-report">รายงานเปรียบเทียบ</a></li>
                         <li><a href="/admin/price-report">รายงานราคา</a></li>
                         <!-- <li><a href="/admin/img-report">รายงานคุณภาพลูกค้า</a></li> -->
-                        <li><a href="/admin/success-report">รายงานสำเร็จ</a></li>
+                        <li><a href="/admin/success-report">รายงานเพิ่มเติม 1</a></li>
                     </ul>
                 </li>
                 <li><a href="/admin/member" class=" waves-effect"><i class="feather-user"></i><span>จัดการสมาชิก</span></a></li>
